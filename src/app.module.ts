0cfec919815ab00dc3d82923ac0addc366a9b8f4
Caio de Araújo <caiomatenorio@gmail.com>
2025-04-18 19:55:20 -0300

    fix: add AuthGuard as a global guard in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { PrismaModule } from './prisma/prisma.module'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 import { ScheduleModule } from '@nestjs/schedule'
+import { APP_GUARD } from '@nestjs/core'
+import { AuthGuard } from './auth/auth.guard'
 
 @Module({
   imports: [PrismaModule, AuthModule, UsersModule, ScheduleModule.forRoot()],
+  providers: [{ provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AppModule {}
