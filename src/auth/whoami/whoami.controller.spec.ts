969b22a5d7fe7d210bf1b0732a2ebe56a9b857fc
Caio de Araújo <caiomatenorio@gmail.com>
2025-04-19 14:52:24 -0300

    feat: add WhoamiController and StatusController with corresponding tests

---
+++ src/auth/whoami/whoami.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { WhoamiController } from './whoami.controller';
+
+describe('WhoamiController', () => {
+  let controller: WhoamiController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [WhoamiController],
+    }).compile();
+
+    controller = module.get<WhoamiController>(WhoamiController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
